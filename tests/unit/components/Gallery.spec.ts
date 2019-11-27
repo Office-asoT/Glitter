9027080f0e5211b1861b50c5cbf311520b930fde
p-baleine <p.baleine@gmail.com>
2019-11-27 19:31:36 +0900

    test pass

--- tests/unit/components/Gallery.spec.ts
+++ tests/unit/components/Gallery.spec.ts
@@ -7,35 +7,48 @@ import PrevArrow from '@/components/buttons/PrevArrow.vue';
 describe('Gallery.vue', () => {
   describe('propsのisOpened=trueのとき', () => {
     it('display != noneであること', () => {
-      const propsData = { isOpened: true };
+      const propsData = props({ isOpened: true });
       const wrapper = shallowMount(Gallery, { propsData });
       expect(wrapper.isVisible()).toBe(true);
     });
 
     it('propsのhasNextをNextArrowにわたしていること', () => {
-      const propsData = { isOpened: true, hasNext: true };
+      const propsData = props({ isOpened: true, hasNext: true });
       const wrapper = shallowMount(Gallery, { propsData });
       expect(wrapper.find(NextArrow).props().isVisible).toBeTruthy();
     });
 
     it('propsのhasPrevをPrevArrowにわたしていること', () => {
-      const propsData = { isOpened: true, hasPrev: true };
+      const propsData = props({ isOpened: true, hasPrev: true });
       const wrapper = shallowMount(Gallery, { propsData });
       expect(wrapper.find(PrevArrow).props().isVisible).toBeTruthy();
     });
 
     describe('repeatImages=trueのとき', () => {
       it('hadNextの値に限らずNextArrow.isVisibleにtrueをわたしていること', () => {
-        const propsData = { isOpened: true, hasNext: false, repeatImages: true };
+        const propsData = props({
+          isOpened: true, hasNext: false, repeatImages: true });
         const wrapper = shallowMount(Gallery, { propsData });
         expect(wrapper.find(NextArrow).props().isVisible).toBeTruthy();
       });
 
       it('hasPrevの値に限らずPrevArrow.isVisibleにtrueをわたしていること', () => {
-        const propsData = { isOpened: true, hasPrev: false, repeatImages: true };
+        const propsData = props({
+          isOpened: true, hasPrev: false, repeatImages: true });
         const wrapper = shallowMount(Gallery, { propsData });
         expect(wrapper.find(NextArrow).props().isVisible).toBeTruthy();
       });
     });
   });
 });
+
+function props(opts: any) {
+  return {
+    isOpened: true,
+    hasNext: false,
+    hasPrev: false,
+    imageItems: [],
+    repeatImages: false,
+    ...opts,
+  };
+}
